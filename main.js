b79c259f980cb433745b39273f8fd8f9a6859afa
Александр <madmandarin.tk@gmail.com>
2016-11-02 21:29:58 +0500

    подключение гитхаба

--- main.js
+++ main.js
@@ -1,1 +1,47 @@
-console.log('test');
+ = function(option) {
+/* ---------------------------- begin view ----------------------------- */
+	'use strict';
+
+	var view = {
+		
+	};
+/* ----------------------------- end view ------------------------------ */
+	
+/* ---------------------------- begin model ---------------------------- */
+	
+	var model = {
+		
+	};
+/* ----------------------------- end model ----------------------------- */
+/* -------------------------- begin controller ------------------------- */
+	
+	var controller = {
+		
+	};
+/* --------------------------- end controller -------------------------- */
+/* --------------------- anonymous initialize function ----------------- */
+	(function() {
+		var app = {
+			init: function () {
+				this.main();
+				this.event();
+			},
+			main: function () {
+			},
+			event: function () {
+				document.addEventListener("DOMContentLoaded", controller.start);
+			}
+		};
+		
+		app.init();
+	}());
+/* --------------------- anonymous initialize function ----------------- */
+/* --------------------- returned object ----------------- */
+	var obj = {
+		scrollTo: controller.scrollingTo,
+		start: controller.start,
+	};
+	return obj;
+/* --------------------- end returned object ----------------- */
+	
+};
