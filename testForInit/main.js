39b2753cf1435eb8343e0bc16d9836fb80ec333f
Александр Кудрявцев <MadMandarin.tk@gmail.com>
2017-03-03 22:35:01 +0500

    init 0.9

--- testForInit/main.js
+++ testForInit/main.js
@@ -1,4 +1,8 @@
 'use strict';
+
+var links = ['Table1.json', 'Table2.json']
+
+
 var testFrontEnd = angular.module('testFrontEnd', []);
 
 
@@ -37,5 +41,25 @@ testFrontEnd.controller('tableCtrl', ['$scope', '$http', function($scope, $http)
 		console.log(response);
 	});
 
+	var table = []
 	
+	links.forEach(function(item, i, arr) {
+		$http({
+		method: 'GET',
+		url: 'item'
+		}).then(function successCallback(response) {
+			for (var data in response.data) {
+				var obj = {
+					name: data,
+					data: response.data[data]
+				}
+				table.push(obj);
+			}
+			
+		}, function errorCallback(response) {
+			console.log(response);
+		});
+	});
+
+	console.log(table);
 }]);
