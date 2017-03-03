ebc02580675355b3e1eff43c6008fb83a4577b1a
Александр Кудрявцев <MadMandarin.tk@gmail.com>
2017-03-03 22:00:41 +0500

    init

---
+++ testForInit/main.js
@@ -0,0 +1,42 @@
+'use strict';
+var testFrontEnd = angular.module('testFrontEnd', ['ngResource']);
+
+
+/* Factory */
+testFrontEnd.factory('Data', ['$resource', function($resource){
+	return $resource(':table.:format', {
+		table: 'Table1',
+		format: 'json'
+	});
+}])
+
+
+
+/* Controllers */
+testFrontEnd.controller('tableCtrl', ['$scope', '$http', 'Data', function($scope, $http, Data){
+	$scope.sortField = undefined;
+	$scope.reverse = false;
+
+	$scope.sort = function(fieldName) {
+		if ($scope.sortField == fieldName) {
+			$scope.reverse = $scope.reverse ? false:true;
+		} else {
+			$scope.sortField = fieldName;
+			$scope.reverse = false;
+		}
+	}
+
+	$scope.isSort = function(fieldName) {
+		return $scope.sortField != fieldName;
+	}
+
+	$scope.isSortUp = function(fieldName) {
+		return $scope.sortField === fieldName && !$scope.reverse;
+	}
+
+	$scope.isSortDown = function(fieldName) {
+		return $scope.sortField === fieldName && $scope.reverse;
+	}
+
+	$scope.data = Data.query();
+}]);
