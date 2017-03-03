7984ee27e9000ccfa466ef78a5964134d62147a7
Александр Кудрявцев <MadMandarin.tk@gmail.com>
2017-03-04 02:05:01 +0500

    мелкие правки

--- testForInit/main.js
+++ testForInit/main.js
@@ -7,6 +7,7 @@ var testFrontEnd = angular.module('testFrontEnd', []);
 
 /* Controllers */
 testFrontEnd.controller('tableCtrl', ['$scope', '$http', function($scope, $http){
+	// По очерёдная загрузка json'в в память, правка структуры, и запись их в $scope.table
 	$scope.table = []
 	links.forEach(function(item, i, arr) {
 		$http({
@@ -14,7 +15,7 @@ testFrontEnd.controller('tableCtrl', ['$scope', '$http', function($scope, $http)
 		url: item
 		}).then(function successCallback(response) {
 			for (var key in response.data) {
-				var obj = {
+				var obj = { 
 					name: key,
 					data: response.data[key]
 				}
@@ -26,20 +27,24 @@ testFrontEnd.controller('tableCtrl', ['$scope', '$http', function($scope, $http)
 		});
 	});
 
-	$scope.sortField = undefined;
-	$scope.reverse = false;
+
+	// Функции отвечающие за сортировка таблицы
+	$scope.sortField = undefined; // текущий отсортированный столбец
+	$scope.reverse = false; // реверс сортировки
 
 	$scope.sort = function(fieldName) {
 		if ($scope.sortField == fieldName) {
-			$scope.reverse = $scope.reverse ? false:true;
+			$scope.reverse = !$scope.reverse;
 		} else {
 			$scope.sortField = fieldName;
 			$scope.reverse = false;
 		}
 	}
 
+
+	// Три функции отвечающие за маркеры сортировки для столбцов
 	$scope.isSort = function(fieldName) {
-		return $scope.sortField != fieldName;
+		return $scope.sortField === fieldName;
 	}
 
 	$scope.isSortUp = function(fieldName) {
@@ -50,13 +55,16 @@ testFrontEnd.controller('tableCtrl', ['$scope', '$http', function($scope, $http)
 		return $scope.sortField === fieldName && $scope.reverse;
 	}
 
-	$scope.activeTable = undefined;
+
+	// Функции отвечающие за переключение таблиц
+	$scope.activeTable = undefined; // id текущей активной таблицы
 
 	$scope.setTable = function(tableId) {
 		$scope.data = $scope.table[tableId].data;
 		$scope.activeTable = tableId;
 	}
 
+	// функция проверки активности таблицы
 	$scope.isActiveTable = function(tableId) {
 		return $scope.activeTable === tableId;
 	}
