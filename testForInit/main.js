8d976e70d7d9d3361a72ca2286a81356c1d186ee
Александр Кудрявцев <MadMandarin.tk@gmail.com>
2017-03-03 22:45:47 +0500

    init 0.14

--- testForInit/main.js
+++ testForInit/main.js
@@ -2,6 +2,24 @@
 
 var links = ['Table1.json', 'Table2.json']
 
+var table = []
+links.forEach(function(item, i, arr) {
+	$http({
+	method: 'GET',
+	url: item
+	}).then(function successCallback(response) {
+		for (var data in response.data) {
+			var obj = {
+				name: data,
+				data: response.data[data]
+			}
+			table.push(obj);
+		}
+		
+	}, function errorCallback(response) {
+		console.log(response);
+	});
+});
 
 var testFrontEnd = angular.module('testFrontEnd', []);
 
@@ -32,31 +50,11 @@ testFrontEnd.controller('tableCtrl', ['$scope', '$http', function($scope, $http)
 		return $scope.sortField === fieldName && $scope.reverse;
 	}
 
-	var table = []
-	
-	links.forEach(function(item, i, arr) {
-		$http({
-		method: 'GET',
-		url: item
-		}).then(function successCallback(response) {
-			for (var data in response.data) {
-				var obj = {
-					name: data,
-					data: response.data[data]
-				}
-				table.push(obj);
-			}
-			
-		}, function errorCallback(response) {
-			console.log(response);
-		});
-	});
+	$scope.activeTable = 0;
+	$scope.data = table[$scope.activeTable].data;
 
-	$scope.activeTable = undefined;
 	$scope.setTable = function(tableId) {
 		$scope.data = table[tableId].data;
 		$scope.activeTable = tableId;
 	}
-
-	$scope.setTable(0)
 }]);
