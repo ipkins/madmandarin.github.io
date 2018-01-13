24d9c3bb855b57aec4a303d5efa82098c68338a0
MadMandarin <me@madmandarin.info>
2018-01-13 08:22:24 +0500

    Список игроков на сервере майнкрафт бета 0.1

--- backend/web.py
+++ backend/web.py
@@ -1,6 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
+import mcrcon
 
 app = Flask(__name__)
+
+@app.route('/rest/minecraft/list')
+def status_json():
+    rcon = mcrcon.MCRcon()
+    rcon.connect('localhost', 27014, 271271)
+    test = rcon.command('/list')
+    data = ''
+    if test:
+        data = test
+    return jsonify({
+        "test": data
+    })
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
