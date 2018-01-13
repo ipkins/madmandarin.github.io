e7fee9a9a9b15975e83a9c934255bd27575bc586
MadMandarin <me@madmandarin.info>
2018-01-13 08:42:06 +0500

    Список игроков на сервере майнкрафт бета 0.4

--- backend/web.py
+++ backend/web.py
@@ -13,5 +13,47 @@ def status_json():
         "users": sorted(x.strip() for x in data.split(':')[1].split(','))
     })
 
+@app.route('/rest/minecraft/ban')
+def status_json():
+    if request.method == 'POST':
+        if request.form['password'] == 'kWb1QY8d':
+        rcon = mcrcon.MCRcon()
+        rcon.connect('localhost', 27014, '271271')
+        data = rcon.command('/ban ' + request.form['nick'])
+    else:
+        data = 'Ошибка авторизации'
+
+    return jsonify({
+        "result": data;
+    })
+
+@app.route('/rest/minecraft/kick')
+def status_json():
+    if request.method == 'POST':
+        if request.form['password'] == 'kWb1QY8d':
+        rcon = mcrcon.MCRcon()
+        rcon.connect('localhost', 27014, '271271')
+        data = rcon.command('/kick ' + request.form['nick'])
+    else:
+        data = 'Ошибка авторизации'
+
+    return jsonify({
+        "result": data;
+    })
+
+@app.route('/rest/minecraft/restart')
+def status_json():
+    if request.method == 'POST':
+        if request.form['password'] == 'kWb1QY8d':
+        rcon = mcrcon.MCRcon()
+        rcon.connect('localhost', 27014, '271271')
+        data = rcon.command('/stop')
+    else:
+        data = 'Ошибка авторизации'
+
+    return jsonify({
+        "result": data;
+    })
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
